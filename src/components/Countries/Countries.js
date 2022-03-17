bc1d359688d22a82e1d78c7f8acf201b9b979268
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-17 12:05:46 +0600

    rest countries using reactjs with yarn

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+import './Countries.css'
+
+const Countries = () => {
+
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+  },[])
+
+
+  return (
+    <div>
+      <h1>Hello fromo countries: {countries.length}</h1>
+    {
+      countries.map(country => <Country country = {country.name.common}></Country>)
+    }
+
+    </div>
+  );
+};
+
+export default Countries;
---
+++ src/components/Country/Country.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Country = (props) => {
+  return (
+    <div>
+      <h2>Country Name: {props.country}</h2>
+    </div>
+  );
+};
+
+export default Country;
