f06fadc22a87185a1950bfa2374b7f1f63e8905d
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-17 15:19:02 +0600

    add region, area, population & map

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -3,14 +3,16 @@ import './Country.css';
 
 const Country = (props) => {
   // use distructuring 
-  const {name, capital, population, region, area} = props.country
+  const {name, capital, population, region, area, flags, maps} = props.country
   return (
     <div className='container-country'>
+      <img src={flags.png} alt='' />
       <h3> <h2>Country:</h2> {name.common}</h3>
       <p>Capital: {capital}</p>
       <p>Population: {population}</p>
       <p>Region: {region}</p>
       <p>Area: {area}</p>
+      <a href={maps.googleMaps} target="_blank">Visit Map</a>
     </div>
   );
 };
